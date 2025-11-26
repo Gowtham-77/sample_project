8f2aee980904e96812bbd7f2179e1770ef40c416
Satish <savvunaragani73@gmail.com>
2025-11-26 12:02:16 +0530

    firt commit

--- mockauthentication/src/main/java/com/example/mockauthentication/MockauthenticationApplication.java
+++ mockauthentication/src/main/java/com/example/mockauthentication/MockauthenticationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
+public class	
 MockauthenticationApplication {
 
 	public static void main(String[] args) {
